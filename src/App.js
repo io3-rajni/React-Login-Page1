9f337d52bf443119ca3de512f3ee81bba694b296
Rajni <io3.rajni@gmail.com>
2023-07-21 19:42:32 +0530

    ropp

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Home from "./component/Home";
 import Signin from "./component/Signin";
 import Profile from "./component/Profile";
 import ChangeName from "./component/ChangeName";
+import { useState } from "react";
 // import Service from "./component/Service";
 
 function App() {
+  const [color, setColor] = useState('red')
+  const [bgColor, setBgColor] = useState("green")
+  const [childDataInParent, setChildDataInParent] = useState('')
+  const parentFunction = (data) => {
+    console.log('childData', data)
+    setChildDataInParent(data)
+  }
+  // parentFunction('data this is Parent')
   return (
     <div className="main-class">
       <BrowserRouter>
@@ -19,7 +28,14 @@ function App() {
           {/* <Route path="/service" element={<Service/>}></Route> */}
         </Routes>
       </BrowserRouter>
-      < ChangeName />
+      <h2>{childDataInParent}</h2>
+      < ChangeName
+        colorProps={color}
+        setColor={setColor}
+        setBgColor={setBgColor}
+        bgColor={bgColor}
+        parentFunction={parentFunction}
+      />
     </div>
   );
 }
--- src/component/ChangeName.js
+++ src/component/ChangeName.js
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
-const ChangeName = () => {
+const ChangeName = (props) => {
+    console.log('setBgColor', props)
+    const { colorProps, bgColor, setBgColor, setColor, parentFunction } = props
     const [name, setName] = useState("Rajni Maurya")
+    const [childData, setChildData] = useState('Child Data')
+    // const [nameReset, setNameReset] = useState("")
     const changeName = () => {
         setName("Ashu")
+        setBgColor('black')
+        setColor('white')
+        parentFunction(childData)
     }
     const reChangeName = () => {
         setName("Maurya")
     }
     const reset = () => {
+        // setNameReset("")
         setName("")
     }
     const preName = () => {
         setName("Rajni Maurya")
     }
-    return <>
-        <diiv>
-            <h1>Enter Your Name</h1>
-            <h2>{name}</h2>
-            {/* <h2> {nameReset}</h2> */}
-            <button onClick={changeName}>Name</button>
-            <button onClick={reChangeName}>Change Button</button>
-            <button onClick={reset}>Reset</button>
-            <button onClick={preName}>PreName             
-            </button>
 
-        </diiv>
-    </>
+    // console.log('color', colorProps)
+    return <div>
+        <h1>Enter Your Name</h1>
+        <h2>{name}</h2>
+        {/* <h2> {nameReset}</h2> */}
+        <button onClick={changeName} style={{ color: colorProps, background: bgColor }}>Name</button>
+        <button onClick={reChangeName}>Change Button</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={preName}>PreName
+
+
+        </button>
+
+    </div>
 
 }
 export default ChangeName
